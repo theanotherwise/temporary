e6e294e0163941cd9c1b10efbfe9edea43b08404
Mateusz Adam Katana <theanotherwise@zeta.theanotherwise.com>
2020-05-14 16:45:14 +0200

    #

--- remove-containers.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-
-docker container stop `docker ps -a | tail -n +2 | awk '{print $1}'`
-docker container rm `docker ps -a | tail -n +2 | awk '{print $1}'`
--- remove-images.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker image rm `docker images -a | tail -n +2 | awk '{print $3}'`
